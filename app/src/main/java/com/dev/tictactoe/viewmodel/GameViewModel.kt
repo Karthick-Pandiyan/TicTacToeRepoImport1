80e24fcb77576323ff3bbaa2e7f78d2ba6405098
2020-DEV-058 <>
2020-03-05 18:31:01 +0530

    [Created test case] To test given function should return player one value, when 0,0 column clicked.

--- app/src/main/java/com/dev/tictactoe/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/dev/tictactoe/viewmodel/GameViewModel.kt
@@ -3,6 +3,7 @@ package com.dev.tictactoe.viewmodel
 import androidx.databinding.ObservableArrayMap
 import com.dev.tictactoe.model.Cell
 import com.dev.tictactoe.model.Game
+import com.kp.tictactoe.utilities.StringUtility
 
 class GameViewModel {
 
@@ -17,6 +18,7 @@ class GameViewModel {
     fun onClickedCellAt(row: Int, column: Int) {
         if (game.cells[row][column].isEmpty) {
             game.cells[row][column] = Cell(game.currentPlayer)
+            cells[StringUtility.stringFromNumbers(row, column)] = game.currentPlayer.value
         }
     }
 }
--- app/src/test/java/com/dev/tictactoe/viewmodel/GameViewModelTest.kt
+++ app/src/test/java/com/dev/tictactoe/viewmodel/GameViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.dev.tictactoe.viewmodel
 
+import com.kp.tictactoe.utilities.StringUtility
 import org.junit.Assert
 import org.junit.Test
 
@@ -8,6 +9,7 @@ class GameViewModelTest {
     private val viewModel = GameViewModel()
     private val playerOne = "John"
     private val playerTwo = "Harry"
+    private val playerOneValue = "X"
 
     @Test
     fun `Given function should return player one name`(){
@@ -38,6 +40,18 @@ class GameViewModelTest {
         viewModel.onClickedCellAt(0,0)
 
         val actualResult =  viewModel.game.cells[0][0].player?.name
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Given function should return player one value, when 0,0 column clicked in the game`(){
+        val expectedResult = playerOneValue
+
+        viewModel.init(playerOne, playerTwo)
+        viewModel.onClickedCellAt(0,0)
+
+        val actualResult =  viewModel.cells[StringUtility.stringFromNumbers(0, 0)]
 
         Assert.assertEquals(expectedResult, actualResult)
     }
