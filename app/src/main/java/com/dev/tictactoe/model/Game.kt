e8eda0fd657a5b5cf3586ad4231455168c75059d
2020-DEV-058 <>
2020-03-05 12:45:44 +0530

    [Created test case] To test given function should return player 2 when switching player from player 1.

--- app/src/main/java/com/dev/tictactoe/model/Game.kt
+++ app/src/main/java/com/dev/tictactoe/model/Game.kt
@@ -4,7 +4,10 @@ class Game(var playerOne: String, var playerTwo: String) {
 
     val player1 =  Player(playerOne, "X")
     val player2 = Player(playerTwo, "O")
-    val currentPlayer = player1
+    var currentPlayer = player1
 
+    fun switchPlayer() {
+        currentPlayer = if (currentPlayer == player1) player2 else player1
+    }
 
 }
--- app/src/test/java/com/dev/tictactoe/model/GameTest.kt
+++ app/src/test/java/com/dev/tictactoe/model/GameTest.kt
@@ -6,9 +6,11 @@ import org.junit.Test
 class GameTest {
     private val playerOne = "John"
     private val playerTwo = "Harry"
-    private val playerValue = "X"
+    private val playerOneValue = "X"
+    private val playerTwoValue = "O"
     private val game = Game(playerOne, playerTwo)
-    val player1 = Player(playerOne, playerValue)
+    private val player1 = Player(playerOne, playerOneValue)
+    private val player2 = Player(playerTwo, playerTwoValue)
 
 
     @Test
@@ -50,6 +52,17 @@ class GameTest {
     @Test
     fun `Given function should return current player name as player one name, when game starts on first time`(){
         val expectedResult = player1.name
+
+        val actualResult = game.currentPlayer.name
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Given function should return player 2 when switching player, if current player is player 1`(){
+        val expectedResult = player2.name
+
+        game.switchPlayer()
 
         val actualResult = game.currentPlayer.name
 
