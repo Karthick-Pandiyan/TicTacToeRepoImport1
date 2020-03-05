afedfea20319c07ac3d71744c0fea3a6643c5694
2020-DEV-058 <>
2020-03-05 17:41:31 +0530

    [Created test case] To test given function should return expected result.

--- app/src/main/java/com/dev/tictactoe/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/dev/tictactoe/viewmodel/GameViewModel.kt
@@ -1,12 +1,15 @@
 package com.dev.tictactoe.viewmodel
 
+import androidx.databinding.ObservableArrayMap
 import com.dev.tictactoe.model.Game
 
 class GameViewModel {
 
     lateinit var game: Game
+    lateinit var cells: ObservableArrayMap<String, String>
 
     fun init(playerOne: String, playerTwo: String){
         game = Game(playerOne, playerTwo)
+        cells = ObservableArrayMap()
     }
 }
