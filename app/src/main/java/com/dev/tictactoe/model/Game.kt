a16eb8141a6d3e4ca78ff75c0e2ea80b0ae9ea1e
2020-DEV-058 <>
2020-03-05 12:27:06 +0530

    [Created test case] To test given function should return expected result, when player two is configured.

--- app/src/main/java/com/dev/tictactoe/model/Game.kt
+++ app/src/main/java/com/dev/tictactoe/model/Game.kt
@@ -3,4 +3,6 @@ package com.dev.tictactoe.model
 class Game(var playerOne: String, var playerTwo: String) {
 
     val player1 =  Player(playerOne, "X")
+    val player2 = Player(playerTwo, "O")
+
 }
