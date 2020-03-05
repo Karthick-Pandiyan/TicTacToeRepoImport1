1257937594ee8a6edeccd42c65898af61080d177
2020-DEV-058 <>
2020-03-05 13:00:54 +0530

    [Created test case] To test given function should return expected player name, when player one passed.

---
+++ app/src/test/java/com/dev/tictactoe/model/CellTest.kt
@@ -0,0 +1,21 @@
+package com.dev.tictactoe.model
+
+import org.junit.Assert
+import org.junit.Test
+
+class CellTest {
+
+    private val playerOne = "John"
+    private val playerOneValue = "X"
+    private val player1 = Player(playerOne, playerOneValue)
+    private val cell = Cell(player1)
+
+    @Test
+    fun `Given function should return expected player name, when player one passed`(){
+        val expectedResult = playerOne
+
+        val actualResult = cell.player?.name
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+}
